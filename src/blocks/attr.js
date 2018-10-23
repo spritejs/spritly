8e14600ad462a998329e8179bfbf7d7cc117dcd2
akira-cn <akira.cn@gmail.com>
2018-10-23 11:37:28 +0800

    update

--- src/blocks/attr.js
+++ src/blocks/attr.js
@@ -30,7 +30,7 @@ Blockly.JavaScript.field_attr_inc = function (block) {
   return [`v => v ${op} ${value}`, Blockly.JavaScript.ORDER_MEMBER];
 };
 
-function createKVConf(keys = 'key', valueType = '') {
+function createKVConf(keys = 'key', valueType = '', colour = Blockly.Msg.ATTRS_HUE, statementType = 'KeyValue') {
   const arg0 = {name: 'KEY', type: 'field_input'};
   if(Array.isArray(keys)) {
     arg0.type = 'field_dropdown';
@@ -45,8 +45,8 @@ function createKVConf(keys = 'key', valueType = '') {
       {type: 'input_value', name: 'VALUE', check: valueType},
     ],
     colour,
-    previousStatement: 'KeyValue',
-    nextStatement: 'KeyValue',
+    previousStatement: statementType,
+    nextStatement: statementType,
   };
 }
 
@@ -58,12 +58,6 @@ Blockly.Blocks.field_attr = {
     });
   },
 };
-
-function gencode(block) {
-  const key = block.getFieldValue('KEY');
-  const value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ADDITION) || 'null';
-  return `\n'${key}': ${value},`;
-}
 
 Blockly.Blocks.field_attr_xy = {
   init() {
@@ -83,7 +77,29 @@ Blockly.Blocks.field_attr_bgcolor = {
   },
 };
 
-Blockly.JavaScript.field_attr = gencode;
-Blockly.JavaScript.field_attr_xy = gencode;
-Blockly.JavaScript.field_attr_size = gencode;
-Blockly.JavaScript.field_attr_bgcolor = gencode;
+Blockly.Blocks.field_attr_rotate = {
+  init() {
+    this.jsonInit(createKVConf('rotate', 'Number'));
+  },
+};
+
+Blockly.Blocks.field_attr_text = {
+  init() {
+    this.jsonInit(createKVConf('text', 'String', Blockly.Msg.ATTRS_HUE + 25));
+  },
+};
+
+function gencode(block) {
+  const key = block.getFieldValue('KEY');
+  const value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ADDITION) || 'null';
+  return `\n'${key}': ${value},`;
+}
+
+Object.assign(Blockly.JavaScript, {
+  field_attr: gencode,
+  field_attr_xy: gencode,
+  field_attr_size: gencode,
+  field_attr_bgcolor: gencode,
+  field_attr_rotate: gencode,
+  field_attr_text: gencode,
+});
